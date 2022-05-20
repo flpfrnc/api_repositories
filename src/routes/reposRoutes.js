e05e65e5302812a0d12fb70aa2e710408d205b1b
Felipe França <felipe.franca.108@ufrn.edu.br>
2022-05-20 14:28:50 -0300

    correcting oldest repository route's name

--- src/routes/reposRoutes.js
+++ src/routes/reposRoutes.js
@@ -6,6 +6,6 @@ const controllers = require("../controllers/reposControllers")
 router.get("/", controllers.home)
 router.get("/blip/", controllers.getAllRepos)
 router.get("/blip/:language", controllers.getBlipReposByLang)
-router.get("/latest", controllers.getLastCSRepos)
+router.get("/oldest", controllers.getOldestCSRepos)
 
 module.exports = router
